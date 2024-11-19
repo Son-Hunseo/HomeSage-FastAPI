761366fdec2e8fea0bd278cfe6467ac7db16dbfd
Hunseo Son <2423aa@naver.com>
2024-11-19 10:53:20 +0900

    feat: get api-key from yaml to env

--- util/keys/api_key.py
+++ util/keys/api_key.py
@@ -1,12 +1,4 @@
-import yaml
 import os
-
-# for local
-# def get_openai_apikey(file_path : str = "./secrets.yaml"):
-#     with open(file_path, 'r') as file:
-#         secrets = yaml.safe_load(file)
-#         openai_api_key = secrets['openai_api_key']
-#     return openai_api_key
 
 def get_openai_apikey():
     openai_api_key = os.environ.get('OPENAI_API_KEY')
